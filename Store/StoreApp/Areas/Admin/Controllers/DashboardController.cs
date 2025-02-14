9bd894fb288ec701aed556cb0d5d2b0487292ef4
Hamit Bozdag <bozdag.013@gmail.com>
2025-02-14 05:57:34 +0300

    update

--- Store/StoreApp/Areas/Admin/Controllers/DashboardController.cs
+++ Store/StoreApp/Areas/Admin/Controllers/DashboardController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace StoreApp.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class DashboardController : Controller
     {
         public IActionResult Index()
