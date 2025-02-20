d86b2999038a7109e70830083a55ed59b71b0400
Hamit Bozdag <bozdag.013@gmail.com>
2025-02-20 12:48:22 +0300

    update

--- Store/Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
+++ Store/Entities/obj/Debug/net6.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4522aa3dd89a55c0278a134fb624359e448467aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e4dab4357757ea50e98afb18e773a84fa4b01d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Store/StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
+++ Store/StoreApp/obj/Debug/net6.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4522aa3dd89a55c0278a134fb624359e448467aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e4dab4357757ea50e98afb18e773a84fa4b01d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
